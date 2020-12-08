21b279ca1a7466f3971ea9bb4f06068284fb1314
nekogee <jessyhui1998@gmail.com>
2020-12-09 00:10:29 +0800

    周三-12-09_0010_post

--- publish.cmd
+++ publish.cmd
@@ -2,9 +2,8 @@
 hugo -t anybodyhome
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
-echo %mydate%_%mytime%
-SET TIME=%mydate%_%mytime%_post
+SET COMMIT=%mydate%_%mytime%_post
 git add .
-git commit -m TIME
+git commit -m %COMMIT%
 git push -u origin main
 cmd
